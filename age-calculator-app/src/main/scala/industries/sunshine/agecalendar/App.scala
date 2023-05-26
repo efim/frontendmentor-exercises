467b52ce10e26685ecd73113ac0cf5a2f9d1b7a1
efim <efim.nefedov@nordigy.ru>
2023-05-26 22:34:08 +0000

    feat: separator desktop lg: styling

--- age-calculator-app/src/main/scala/industries/sunshine/agecalendar/App.scala
+++ age-calculator-app/src/main/scala/industries/sunshine/agecalendar/App.scala
@@ -44,19 +44,23 @@ object Main {
 
   def renderSeparator(): Element = {
     div(
-      className := "py-4 w-10/12 lg:w-full",
+      className := "py-4 w-10/12 lg:py-0 lg:w-full",
       div(
-        className := "relative z-0 w-full h-8",
+        className := "relative z-0 w-full h-8 lg:h-12",
         div(
-          className := "absolute w-8 h-8 rounded-full transform -translate-x-1/2 bg-main-purple start-1/2"
+          className := "absolute w-8 h-8 rounded-full transform -translate-x-1/2 bg-main-purple start-1/2",
+          className := "lg:w-12 lg:h-12 lg:-translate-x-full lg:start-full"
+
         ),
         div(
           className := "absolute top-1/2 w-full transform -translate-y-1/2 bg-light-grey h-[2px] -z-10"
         ),
         div(
-          img(src := "images/icon-arrow.svg", alt := ""),
+          img(src := "images/icon-arrow.svg", alt := "", className := "lg:w-6 lg:h-6"),
           className := "absolute transform -translate-x-1/2 translate-y-1/2 start-1/2",
-          className := "w-4 h-4"
+          className := "w-4 h-4",
+          // TODO this translate 2.3rem means that icon would be better places 'in the center' of circle div
+          className := "lg:w-8 lg:h-6 lg:-translate-x-[2.3rem] lg:start-full"
         )
       )
     )
