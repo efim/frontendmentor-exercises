961540fdc4927e287816c101221d0a368a376821
efim <efim.nefedov@nordigy.ru>
2023-05-26 21:36:04 +0000

    feat: inputs mobile styling

--- age-calculator-app/src/main/scala/industries/sunshine/agecalendar/InputsComponent.scala
+++ age-calculator-app/src/main/scala/industries/sunshine/agecalendar/InputsComponent.scala
@@ -79,7 +79,7 @@ object InputsComponent {
       )
     }
     div(
-      className := "flex flex-row",
+      className := "flex flex-row justify-between px-3 pt-5 w-full",
       renderDatePartInput(
         "day",
         selectedDate,
@@ -119,7 +119,8 @@ object InputsComponent {
     val inputElement = input(
       idAttr := inputUid,
       placeholder := "24",
-      className := "w-10 text-[0.75rem]",
+      className := "p-2 rounded w-[85px] h-[50px] border-[1px] border-light-grey",
+      className := "w-10 font-medium appearance-none text-[0.65rem] font-fancy-sans",
       typ := "number",
       minAttr.maybe(min.map(_.toString())),
       maxAttr.maybe(max.map(_.toString())),
@@ -130,10 +131,19 @@ object InputsComponent {
       // now this looks like a hack. "attempt to set full date with current states of all three fields"
       onInput.mapToValue --> Observer(_ => setTheDate())
     ).validated(validation)
+
     div(
-      className := "flex flex-col items-start w-10 appearance-none",
-      label(name, forId := inputUid, className := "text-[0.5rem]"),
+      className := "flex flex-col items-start w-10",
+      label(
+        name,
+        forId := inputUid,
+        className := "pb-1 font-medium tracking-widest uppercase text-[0.4rem] text-smokey-grey font-fancy-sans"
+      ),
       inputElement,
+      // div(
+      //   className := "flex flex-row justify-center p-2 w-10 h-7 rounded border-[2px] border-light-grey",
+      //   inputElement
+      // ),
       child.maybe <-- inputElement.validationError.optionMap(errors =>
         span(
           cls := "text-red-700 text-[0.5rem]",
