bda8f95cf20557a9eb1a861b5cda7e351440921a
efim <efim.nefedov@nordigy.ru>
2023-05-29 19:12:15 +0000

    feat: submit message with Ctrl+Enter

--- 6-interactive-comment-section/src/main/scala/industries/sunshine/interactivecommentsection/MessageInputUI.scala
+++ 6-interactive-comment-section/src/main/scala/industries/sunshine/interactivecommentsection/MessageInputUI.scala
@@ -4,10 +4,19 @@ import com.raquo.laminar.api.L.{*, given}
 import industries.sunshine.interactivecommentsection.Models._
 import com.softwaremill.quicklens._
 
-object  MessageInputUI {
+object MessageInputUI {
 
-  def render(selfUser: AppUser, shouldShowReply: Observer[Boolean], onSubmit: String => Unit): Element = {
+  def render(
+      selfUser: AppUser,
+      shouldShowReply: Observer[Boolean],
+      onSubmit: String => Unit
+  ): Element = {
     val replyText = Var("")
+    val onTextAreaSubmit = Observer { _ =>
+      shouldShowReply.onNext(false)
+      onSubmit(replyText.now())
+      replyText.set("")
+    }
     div(
       className := "grid grid-cols-3 p-4 pb-0 mt-1 bg-white rounded-lg",
       onInput.mapToValue --> replyText,
@@ -17,25 +26,24 @@ object  MessageInputUI {
         controlled(
           value <-- replyText.signal,
           onInput.mapToValue --> replyText.writer
-        )
+        ),
+        onKeyDown.filter { ev =>
+          ev.ctrlKey && ev.key == "Enter"
+        } --> onTextAreaSubmit
       ),
       div(
         className := "flex items-center h-20",
         img(
           src := selfUser.image.png,
           alt := "",
-          className := "w-8 h-8",
-        ),
+          className := "w-8 h-8"
+        )
       ),
       div(
         className := "flex col-start-3 justify-end items-center h-20",
         button(
           className := "w-24 h-12 font-semibold text-white rounded-lg bg-moderate-blue",
-          onClick --> Observer { _ =>
-            shouldShowReply.onNext(false)
-            onSubmit(replyText.now())
-            replyText.set("")
-          },
+          onClick --> onTextAreaSubmit,
           "SEND"
         )
       )
