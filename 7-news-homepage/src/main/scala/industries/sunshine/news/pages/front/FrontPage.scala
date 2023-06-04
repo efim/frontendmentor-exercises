135d46cf0d2014af22787a3919ab7f5ad7247326
efim <efim.nefedov@nordigy.ru>
2023-06-04 06:27:22 +0000

    refactor: moving text into hardcoded model

--- 7-news-homepage/src/main/scala/industries/sunshine/news/pages/front/FrontPage.scala
+++ 7-news-homepage/src/main/scala/industries/sunshine/news/pages/front/FrontPage.scala
@@ -25,44 +25,14 @@ object Main {
   def page(): Element = {
     div(
       className := "font-inter",
-      """
+      div("""
   Home
   New
   Popular
   Trending
   Categories
-
-  The Bright Future of Web 3.0?
-
-  We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
-  But is it really fulfilling its promise?
-
-  Read more
-
-  New
-
-  Hydrogen VS Electric Cars
-  Will hydrogen-fueled cars ever catch up to EVs?
-
-  The Downsides of AI Artistry
-  What are the possible adverse effects of on-demand AI image generation?
-
-  Is VC Funding Drying Up?
-  Private funding by VC firms is down 50% YOY. We take a look at what that means.
-
-  01
-  Reviving Retro PCs
-  What happens when old PCs are given modern upgrades?
-
-  02
-  Top 10 Laptops of 2022
-  Our best picks for various needs and budgets.
-
-  03
-  The Growth of Gaming
-  How the pandemic has sparked fresh opportunities.
-
-"""
+"""),
+      Models.hardcoded.toString()
     )
   }
 
---
+++ 7-news-homepage/src/main/scala/industries/sunshine/news/pages/front/Models.scala
@@ -0,0 +1,58 @@
+package industries.sunshine.news.pages.front
+
+object Models {
+  final case class StoryCard(
+      title: String,
+      description: String,
+      image: String
+  )
+
+  final case class FrontPageState(
+      headliner: StoryCard,
+      newArticles: List[StoryCard],
+      recommended: List[StoryCard]
+  )
+
+  val hardcoded = FrontPageState(
+    headliner = StoryCard(
+      "The Bright Future of Web 3.0?",
+      """  We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+  But is it really fulfilling its promise? """,
+      "url"
+    ),
+    newArticles = List(
+      StoryCard(
+        "Hydrogen VS Electric Cars",
+        "Will hydrogen-fueled cars ever catch up to EVs?",
+        "url"
+      ),
+      StoryCard(
+        "The Downsides of AI Artistry",
+        "What are the possible adverse effects of on-demand AI image generation?",
+        "url"
+      ),
+      StoryCard(
+        "Is VC Funding Drying Up?",
+        "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+        "url"
+      )
+    ),
+    recommended = List(
+      StoryCard(
+        "Reviving Retro PCs",
+        "What happens when old PCs are given modern upgrades?",
+        "url"
+      ),
+      StoryCard(
+        "Top 10 Laptops of 2022",
+        "Our best picks for various needs and budgets.",
+        "url"
+      ),
+      StoryCard(
+        "The Growth of Gaming",
+        "How the pandemic has sparked fresh opportunities.",
+        "url"
+      )
+    )
+  )
+}
