403c91737c909bc1f00ea876f9273c8fc7368201
efim <efim.nefedov@nordigy.ru>
2023-06-04 08:47:55 +0000

    feat: using model in new stories

--- 7-news-homepage/src/main/scala/industries/sunshine/news/pages/front/NewStoriesComponent.scala
+++ 7-news-homepage/src/main/scala/industries/sunshine/news/pages/front/NewStoriesComponent.scala
@@ -6,25 +6,27 @@ import industries.sunshine.news.pages.front.Models.StoryCard
 object NewStoriesComponent {
   def render(newStories: List[StoryCard]): Element = {
     div(
-      className := "p-5 bg-very-dark-blue",
+      className := "px-5 bg-very-dark-blue",
       h1(
-        className := "text-3xl font-bold text-soft-orange",
+        className := "pt-5 text-3xl font-bold text-soft-orange",
         "New"
       ),
       div(
-        className := "grid text-white divide-y",
-        div("story1"),
-        div("story2"),
-        div("story3")
+        className := "grid grid-cols-1 text-white divide-y divide-grayish-blue",
+        newStories.map(renderStoryHeadline(_))
       )
     )
   }
 
-  private def renderStoryHeadline(storySignal: StoryCard): Element = {
-        div(
-          className := "grid",
-          h1( storySignal.title),
-          p( storySignal.description),
-        )
+  private def renderStoryHeadline(story: StoryCard): Element = {
+    div(
+      className := "grid py-6",
+      h1(
+        className := "pb-2 text-xl font-extrabold",
+        story.title),
+      p(
+        className := "text-grayish-blue",
+        story.description)
+    )
   }
 }
