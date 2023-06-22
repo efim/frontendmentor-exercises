ed67660941fdb9734539283ab8fc09790c172054
efim <efim.nefedov@nordigy.ru>
2023-06-22 05:05:06 +0000

    init: adding style guide to taliwind config
    font, colors, font size

--- 10-todo-list/tailwind.config.js
+++ 10-todo-list/tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.scala",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Josefin Sans', 'sans-serif'], // This will set Roboto as the default sans font
+      },
+      fontWeight: {
+        'normal': 400,
+        'bold': 700,
+      },
+      colors: {
+        'primay-bright-blue': 'hsl(220, 98%, 61%)',
+        'primary-check-bg-from': 'hsl(192, 100%, 67%',
+        'primary-check-bg-to': 'hsl(280, 87%, 65%)',
+        // light theme
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+        'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+        'light-grayish-blue': 'hsl(233, 11%, 84%)',
+        'dark-grayish-blue': 'hsl(236, 9%, 61%)',
+        'very-dark-grayish-blue': 'hsl(235, 19%, 35%)',
+        // dark theme
+        'dt-very-dark-blue': 'hsl(235, 21%, 11%)',
+        'dt-very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+        'dt-light-grayish-blue': 'hsl(234, 39%, 85%)',
+        'dt-light-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+        'dt-dark-grayish-blue': 'hsl(234, 11%, 52%)',
+        'dt-very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+        'dt-very-dark-grayish-blue': 'hsl(237, 14%, 26%)',
+      },
+    },
   },
   plugins: [],
 }
